f6f608a4edee1e2421837d6fa7d77a24156d7002
JonathanWang <wangjonathan1992@gmail.com>
2017-11-21 23:14:15 -0500

    Greet user, and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(express.static(publicPath));
 io.on("connection", socket => {
   console.log("New user connected");
 
+  socket.emit("newMessage", {
+    from: "Admin",
+    text: "Welcome to the Chat App",
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit("newMessage", {
+    from: "Admin",
+    text: "New user joined",
+    createdAt: new Date().getTime()
+  });
+
   socket.on("createMessage", message => {
     console.log("createMessage", message);
     io.emit("newMessage", {
@@ -21,6 +33,10 @@ io.on("connection", socket => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit("newMessage", {
+    //     from: message.from,
+    //     text: message.text
+    // });
   });
 
   socket.on("disconnect", function() {
